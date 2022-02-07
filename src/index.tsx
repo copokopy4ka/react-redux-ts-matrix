7d83b27323bed2a6fbbd7ff7eb55be3d38150424
copokopy4ka <copokopy4ka739@gmail.com>
2022-02-07 14:37:02 +0200

    added firebase hosting

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import './index.css'
+import './index.css';
+import './firebase'
 
 ReactDOM.render(
     <Provider store={store}>
